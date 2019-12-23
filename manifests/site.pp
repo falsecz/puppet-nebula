0cb2f4814bb6c60cba8e8b32f68b55f33373d30b
Lukas Fuchs <panzelva@gmail.com>
2019-12-23 14:52:31 +0100

    wip

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,12 +13,25 @@ node 'agent2' {
 }
 
 node 'puppet' {
+  service { 'puppetserver':
+    ensure => 'running'
+  }
+
   puppet_authorization::rule { 'auth_certs':
     match_request_path => '^/puppet/v3/file_(content|metadata)s?/nebula_certs',
     match_request_type => 'regex',
     allow              => '*',
     sort_order         => 300,
     path               => '/etc/puppetlabs/puppetserver/conf.d/auth.conf',
+    notify             => Service['puppetserver'],
+  }
+
+  file { '/etc/puppetlabs/puppet/fileserver.conf':
+    ensure  => 'present',
+    owner   => 'puppet',
+    group   => 'puppet',
+    content => "[nebula_certs]\n  path /etc/nebula-ca\n  allow *",
+    notify  => Service['puppetserver'],
   }
 
   class { 'nebula::ca':
@@ -28,7 +41,7 @@ node 'puppet' {
       { host_name => 'svetylko', address => '192.168.33.1/24', public_ip => 'puppet.test' },
       { host_name => 'zidan', address => '192.168.33.7/24', public_ip => 'agent2.test' },
     ],
-    require => Puppet_authorization::Rule['auth_certs']
+    require => [Puppet_authorization::Rule['auth_certs'], File['/etc/puppetlabs/puppet/fileserver.conf']]
   }
 
   class { 'nebula::host':
